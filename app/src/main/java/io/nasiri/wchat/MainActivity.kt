833d0925174a579280d0d392be39f86f254dd170
Matin Nasiri <mnasirii829@gmail.com>
2024-07-29 18:13:06 +0330

    Hilt was added

--- app/src/main/java/io/nasiri/wchat/MainActivity.kt
+++ app/src/main/java/io/nasiri/wchat/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import io.nasiri.wchat.ui.theme.WChatTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/io/nasiri/wchat/WChat.kt
@@ -0,0 +1,7 @@
+package io.nasiri.wchat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WChat : Application()
